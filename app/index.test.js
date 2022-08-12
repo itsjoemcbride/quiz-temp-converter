eb55388c7d6acd1bed89c18261f47073488ecf21
Joe <itsjoemcbride@gmail.com>
2022-08-12 13:33:37 +0100

    Added new tests

--- app/index.test.js
+++ app/index.test.js
@@ -1,5 +1,17 @@
-import { add2Nums } from "./index.js";
+import tryConvert from "./index.js";
 
-test("1 + 1 = 2", () => {
-  expect(add2Nums(1, 1)).toBe(2);
+it("converts from celsius to fahrenheit", () => {
+  expect(tryConvert(100, "toFahrenheit")).toBe("212");
+});
+
+it("converts from fahrenheit to celsius", () => {
+  expect(tryConvert(50, "toCelsius")).toBe("10");
+});
+
+it("converts to celsius with non-numerical input", () => {
+  expect(tryConvert("Jeff", "toCelsius")).toBe("");
+});
+
+it("converts to farenheit with non-numerical input", () => {
+  expect(tryConvert("Jeff", "toFahrenheit")).toBe("");
 });
